f2a448d67169f7410cd7e3475a477eb176730bb3
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-11-05 15:36:36 +0000

    The app now depends on the meetupr package to get the list of groups and map their location using leaflet.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rshinylady
+
+Before running this app, install meetupr from lucy's branch
+
+`devtools::install_github("rladies/meetupr", ref = "lucy")`
+
+then get a meetup API key and set it as an environmental variable
+
+`Sys.setenv(MEETUP_KEY = "YOUR_API_KEY_HERE")`
--- app.R
+++ app.R
@@ -1,11 +1,3 @@
-################################################################################
-# Before running this app, install meetupr from lucy's branch
-# devtools::install_github("rladies/meetupr", ref = "lucy")
-
-# then get a meetup API key and set it as an environmental variable
-# Sys.setenv(MEETUP_KEY = "YOUR_API_KEY_HERE")
-################################################################################
-
 source("chapters_source.R")
 library(shinydashboard)
 library(shiny)
