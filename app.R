f156d0d5b16dc112e66a1f864ce0bf5c48836f8e
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-11-05 15:32:53 +0000

    Moved instraction to top of app.R

--- app.R
+++ app.R
@@ -1,7 +1,15 @@
+################################################################################
+# Before running this app, install meetupr from lucy's branch
+# devtools::install_github("rladies/meetupr", ref = "lucy")
+
+# then get a meetup API key and set it as an environmental variable
+# Sys.setenv(MEETUP_KEY = "YOUR_API_KEY_HERE")
+################################################################################
+
 source("chapters_source.R")
 library(shinydashboard)
 library(shiny)
-
+library(leaflet)
 
 ## ui.R ##
 
@@ -20,15 +28,23 @@ ui <- dashboardPage(
   dashboardBody(
     fluidRow(
       # A static valueBox
-      # valueBox(n_members, "R-Ladies Members", icon = icon("credit-card")),
       valueBox(n_cities, "R-Ladies Cities", icon = icon("map-marker"), color = "purple"),
       valueBox(n_countries, "R-Ladies Countries", icon = icon("map-o"))
       
     ),
     
+    fluidRow(
+      # A static valueBox
+      valueBox(dim(rladies_groups)[1], "R-Ladies groups on meetup.com", icon = icon("glyphicon-blackboard")),
+      valueBox(sum(rladies_groups$members), "R-Ladies members on meetup.com", icon = icon("users"))
+      
+    ),
+    
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard"))
+      tabItem(tabName = "dashboard", 
+              leafletOutput('map'))
+    )
   )
 )  
     
@@ -36,7 +52,15 @@ ui <- dashboardPage(
   
 
   
-server <- function(input, output) { }
+server <- function(input, output) { 
+  
+  output$map <- renderLeaflet({
+    leaflet(data = rladies_groups) %>% 
+      addTiles() %>%
+      addMarkers(~lon, ~lat, label = ~as.character(name))
+  })
+  
+}
 
 
 shinyApp(ui, server)
