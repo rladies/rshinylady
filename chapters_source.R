1994f03ac0c5b449efc7c91af09174a75b6e14e6
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-02-16 19:54:17 -0800

    Use meetupr package and clean up

--- chapters_source.R
+++ chapters_source.R
@@ -2,51 +2,11 @@ library(rvest)
 library(dplyr)
 library(meetupr)
 
-# read the page where the list of chapters is located
-page <- read_html("https://github.com/rladies/starter-kit/blob/master/Current-Chapters.md")
-
-
-## Get the cities of the chapters
-cities <- page %>%
-  html_nodes("#readme strong") %>% 
-  html_text() %>% 
-  tbl_df()
-
-cities_plus_dc <- page %>%
-  html_nodes("h3:nth-child(150) , #readme strong") %>% 
-  html_text() 
-n_cities <- length(cities_plus_dc)
-
-# get the countries of the chapters ----------------------------
-countries <- page %>% 
-  html_nodes("ul+ h2 , p+ h2") %>% 
-  html_text()
-n_countries <- length(countries)
-
-
-# meetup numbers
-# api_key <- readRDS("../../../meetup_api_key.RDS")
-# doc.raw <- RCurl::getURL("https://raw.githubusercontent.com/rladies/starter-kit/master/Current-Chapters.md")
-# meetups <- stringr::str_match_all(doc.raw, "www.meetup.com/(.*?)/")[[1]][,2]
-# meetups <- unique(meetups)
-# 
-# n_members <- sapply(meetups, function(x) meetupr::get_members(x, api_key = api_key))
-
-
 # meetup groups
-api_key <- Sys.getenv("MEETUP_KEY")
-rladies_groups <- get_groups(text = "r-ladies", api_key = api_key)
+api_key <- readRDS("meetup_key.RDS")
+all_rladies_groups <- find_groups(text = "r-ladies", api_key = api_key)
 
 # Cleanup
-rladies_groups <- rladies_groups[grep(pattern = "ladies", 
-                                      x = rladies_groups$urlname,
-                                      ignore.case = TRUE), ]
-rladies_groups <- rladies_groups[grep(pattern = "r", 
-                                      x = rladies_groups$urlname,
-                                      ignore.case = TRUE), ]
-rladies_groups <- rladies_groups[-grep(pattern = "gentlemen",
-                                       x = rladies_groups$urlname,
-                                       ignore.case = TRUE), ]
-rladies_groups <- rladies_groups[-grep(pattern = "night",
-                                       x = rladies_groups$urlname,
-                                       ignore.case = TRUE), ]
+rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|rug", 
+                                          x = all_rladies_groups$urlname,
+                                          ignore.case = TRUE), ]
