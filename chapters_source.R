2921cd44fedc339440787d431cba7039a64a2ee9
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-03-11 21:35:57 -0700

    Add US metrics

--- chapters_source.R
+++ chapters_source.R
@@ -1,5 +1,5 @@
 library(rvest)
-library(dplyr)
+library(tidyverse)
 library(meetupr)
 
 # meetup groups
@@ -10,3 +10,13 @@ all_rladies_groups <- find_groups(text = "r-ladies", api_key = api_key)
 rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|rug", 
                                           x = all_rladies_groups$urlname,
                                           ignore.case = TRUE), ]
+
+# Each country
+groups_usa <- rladies_groups %>% 
+  filter(country == "US")
+created_usa <- groups_usa %>% 
+  mutate(dt_created = substr(created, 1, 10)) %>% 
+  arrange(desc(dt_created)) %>% 
+  select("city", "state", "dt_created", "members")
+  
+  
